b8188200c32a753094859799d1ea41c9e2312109
Felipinho1806 <fcnfelipe04@gmail.com>
2026-04-12 23:15:24 -0300

    Adicionando contagem de Peças de acordo com posicionamento da torre

--- src/App.java
+++ src/App.java
@@ -6,10 +6,19 @@ public class App {
 
         for(int i=0; i<resultado.length; i++) {
             for(int j=0; j<resultado[i].length; j++) {
+                resultado[1][3] = 'I';
+                resultado[5][3] = 'I';
+                resultado[2][7] = 'I';
+                resultado[2][3] = 'T';
                 System.out.print(resultado[i][j] + " ");
+                if(resultado[i][j] == 'T'){
+                    xadrez.contarPecas(i, j);
+                }
             }
             System.out.println();
         }
+
+        System.out.println("Número de peças que a torre poderia capturar na jogada: " + xadrez.numPecas);
 
     }
 }
--- src/Xadrez.java
+++ src/Xadrez.java
@@ -1,7 +1,8 @@
 public class Xadrez {
 
     char [][]matriz = new char[8][];
- 
+    int numPecas = 0;
+
     public char[][] metodo() {
         for(int i=0; i<matriz.length; i++){
             matriz[i] = new char[8];
@@ -10,6 +11,20 @@ public class Xadrez {
             }
         }
         return matriz;
+    }
+
+    public int contarPecas(int x, int y) {
+        for(int i=0; i<matriz[x].length; i++) {
+            if(matriz[x][i] == 'I') {
+                numPecas++;
+            }
+        }
+        for(int i=0; i<matriz[y].length; i++) {
+            if(matriz[i][y] == 'I') {
+                numPecas++;
+            }
+        }
+        return numPecas;
     }
     
 }
